87a2a2a99233cf0d3524ccd90a1ae5e0b2ab0182
rafa-pdes90 <rafa-pdes90@users.noreply.github.com>
2018-06-20 01:36:29 -0300

    small fix on batch

--- compile.bat
+++ compile.bat
@@ -1,1 +1,2 @@
+if not exist .\bin mkdir bin
 javac -cp .\lib\commons-cli-1.4.jar;.\lib\amqp-client-5.3.0.jar;.\lib\commons-lang3-3.7.jar;.\lib\slf4j-api-1.7.25.jar;.\lib\slf4j-simple-1.7.25.jar;. -sourcepath .\src -d .\bin .\src\*.java
